79d539676dfd3b592e34fbed2949ac723b4ae3a2
shayanahmadIlsa <158051652+shayanahmadIlsa@users.noreply.github.com>
2025-03-12 05:54:20 +0500

    updated home page ui

--- screens/home/Home.jsx
+++ screens/home/Home.jsx
@@ -1,46 +1,225 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   View,
   Text,
-  Image,
   TouchableOpacity,
   ScrollView,
-  StyleSheet,
   SafeAreaView,
-  TextInput,
+  Image,
 } from "react-native";
+import styles from "./style";
 import { Ionicons, MaterialIcons, FontAwesome5 } from "@expo/vector-icons";
 
 const HomeScreen = ({ navigation }) => {
   const categories = [
-    {
-      id: 1,
-      name: "Burgers",
-      icon: require("../../assets/burger.png"),
-    },
-    { id: 2, name: "Pizza", icon: require("../../assets/pizza.png") },
-    { id: 3, name: "Soups", icon: require("../../assets/sandwich.png") },
-    {
-      id: 4,
-      name: "Sandwiches",
-      icon: require("../../assets/sandwich.png"),
-    },
+    { id: 1, name: "Burgers", icon: "fast-food", type: "Ionicons" },
+    { id: 2, name: "Pizza", icon: "local-pizza", type: "MaterialIcons" },
+    { id: 3, name: "Soups", icon: "restaurant", type: "MaterialIcons" },
+    { id: 4, name: "Sandwiches", icon: "hamburger", type: "FontAwesome5" },
+    { id: 5, name: "Rice", icon: "restaurant-menu", type: "MaterialIcons" },
   ];
 
-  const deals = [
-    {
-      id: 1,
-      image: require("../../assets/burger.png"),
-      name: "burger 1",
-      price: "250",
-    },
-    {
-      id: 2,
-      image: require("../../assets/burger.png"),
-      name: "burger 2",
-      price: "750",
-    },
-  ];
+  // Deals mapped correctly to each category
+  const allDeals = {
+    Burgers: [
+      {
+        id: 1,
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTeCacsKFVUFTWs2O_nNZ-liKRkUT8eU8zBHQ&s",
+        name: "Classic Burger",
+        price: "250 Rs",
+      },
+      {
+        id: 2,
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQZdBZvTy7j_4VHavJJdqNHZvhjqK2jswnNtQ&s",
+        name: "Cheese Burger",
+        price: "750 Rs",
+      },
+      {
+        id: 3,
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQZdBZvTy7j_4VHavJJdqNHZvhjqK2jswnNtQ&s",
+        name: "Double Cheese Burger",
+        price: "1750 Rs",
+      },
+    ],
+    Pizza: [
+      {
+        id: 4,
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTkb8bnpzQOP0j7G7CFRtJgdI1K2cIVbV-aZQ&s",
+        name: "Pepperoni Pizza",
+        price: "1200 Rs",
+      },
+      {
+        id: 5,
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSqai2uMqz7lv7paTapP6BCRodFuGjP-aGCiw&s",
+        name: "Margherita Pizza",
+        price: "1000 Rs",
+      },
+      {
+        id: 6,
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSqai2uMqz7lv7paTapP6BCRodFuGjP-aGCiw&s",
+        name: "Veggie Supreme Pizza",
+        price: "800 Rs",
+      },
+    ],
+    Soups: [
+      {
+        id: 7,
+        image:
+          "https://www.washingtonpost.com/resizer/B8KyY23GAXsTnpCDIzah7xN-HAA=/arc-anglerfish-washpost-prod-washpost/public/O2ODNLW4ZBFVDBJKIU56FURJYE.jpg",
+        name: "Tomato Soup",
+        price: "300 Rs",
+      },
+      {
+        id: 8,
+        image:
+          "https://cdn.loveandlemons.com/wp-content/uploads/2021/11/minestrone-soup-500x500.jpg",
+        name: "Minestrone Soup",
+        price: "400 Rs",
+      },
+      {
+        id: 9,
+        image:
+          "https://cdn.loveandlemons.com/wp-content/uploads/2021/11/minestrone-soup-500x500.jpg",
+        name: "Special Minestrone Soup",
+        price: "600 Rs",
+      },
+    ],
+    Sandwiches: [
+      {
+        id: 10,
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRtZjpbxxYDaHOCGKff0PM2Rxn2Omn6_2CILQ&s",
+        name: "Club Sandwich",
+        price: "600 Rs",
+      },
+      {
+        id: 11,
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXeQzfGA95Z4wiejenHhUei9U0BxCXViK0Bg&s",
+        name: "Grilled Cheese Sandwich",
+        price: "450 Rs",
+      },
+      {
+        id: 12,
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXeQzfGA95Z4wiejenHhUei9U0BxCXViK0Bg&s",
+        name: "Deluxe Grilled Cheese",
+        price: "650 Rs",
+      },
+    ],
+    Rice: [
+      {
+        id: 13,
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQoIycguxFgTpIN3L00tYQhJ2WkypXj5w_QkQ&s",
+        name: "Biryani",
+        price: "800 Rs",
+      },
+      {
+        id: 14,
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSPB0SshjQzi37NckUqup-HIv1ULwgFFmJExg&s",
+        name: "Fried Rice",
+        price: "650 Rs",
+      },
+      {
+        id: 15,
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSPB0SshjQzi37NckUqup-HIv1ULwgFFmJExg&s",
+        name: "Special Fried Rice",
+        price: "350 Rs",
+      },
+    ],
+  };
+
+  const populars = {
+    Burgers: [
+      {
+        id: 1,
+        image:
+          "https://www.themealdb.com/images/media/meals/vxuyrx1511302687.jpg",
+        name: "Classic Burger",
+        price: "250 Rs",
+      },
+      {
+        id: 2,
+        image:
+          "https://www.themealdb.com/images/media/meals/sxxpst1468569714.jpg",
+        name: "Cheese Burger",
+        price: "750 Rs",
+      },
+    ],
+    Pizza: [
+      {
+        id: 3,
+        image:
+          "https://www.themealdb.com/images/media/meals/wtsvxx1511296896.jpg",
+        name: "Pepperoni Pizza",
+        price: "1200 Rs",
+      },
+      {
+        id: 4,
+        image:
+          "https://www.themealdb.com/images/media/meals/x0lk931587671540.jpg",
+        name: "Margherita Pizza",
+        price: "1000 Rs",
+      },
+    ],
+    Soups: [
+      {
+        id: 5,
+        image:
+          "https://www.themealdb.com/images/media/meals/uuyrrx1487327597.jpg",
+        name: "Tomato Soup",
+        price: "300 Rs",
+      },
+      {
+        id: 6,
+        image:
+          "https://www.themealdb.com/images/media/meals/xrrtss1511815505.jpg",
+        name: "Minestrone Soup",
+        price: "400 Rs",
+      },
+    ],
+    Sandwiches: [
+      {
+        id: 7,
+        image:
+          "https://www.themealdb.com/images/media/meals/xqvyqr1511638875.jpg",
+        name: "Club Sandwich",
+        price: "600 Rs",
+      },
+      {
+        id: 8,
+        image:
+          "https://www.themealdb.com/images/media/meals/vtqxtu1511639216.jpg",
+        name: "Grilled Cheese",
+        price: "450 Rs",
+      },
+    ],
+    Rice: [
+      {
+        id: 9,
+        image: "https://www.themealdb.com/images/media/meals/1529442323.jpg",
+        name: "Biryani",
+        price: "800 Rs",
+      },
+      {
+        id: 10,
+        image: "https://www.themealdb.com/images/media/meals/1529444113.jpg",
+        name: "Fried Rice",
+        price: "650 Rs",
+      },
+    ],
+  };
+
+  const [selectedCategory, setSelectedCategory] = useState("Burgers");
 
   return (
     <SafeAreaView style={styles.container}>
@@ -48,13 +227,15 @@ const HomeScreen = ({ navigation }) => {
         {/* Hero Banner */}
         <View style={styles.heroBanner}>
           <Image
-            source={require("../../assets/pizza-banner.jpg")}
+            source={{
+              uri: "https://www.themealdb.com/images/media/meals/1529446352.jpg",
+            }}
             style={styles.bannerImage}
             resizeMode="cover"
           />
           <View style={styles.bannerOverlay}>
-            <Text style={styles.bannerTitle}>Bacon Bliss Burger</Text>
-            <Text style={styles.bannerPrice}>Only for $8.00</Text>
+            <Text style={styles.bannerTitle}>Delicious Meals</Text>
+            <Text style={styles.bannerPrice}>Starting from 250 Rs</Text>
           </View>
         </View>
 
@@ -64,16 +245,55 @@ const HomeScreen = ({ navigation }) => {
             <Text style={styles.sectionTitle}>Categories</Text>
           </View>
 
-          <View style={styles.categoriesContainer}>
-            {categories.map((category) => (
-              <TouchableOpacity key={category.id} style={styles.categoryItem}>
-                <View style={styles.categoryIconContainer}>
-                  <Image source={category.icon} style={styles.categoryIcon} />
-                </View>
-                <Text style={styles.categoryName}>{category.name}</Text>
-              </TouchableOpacity>
-            ))}
-          </View>
+          <ScrollView
+            horizontal={true}
+            showsHorizontalScrollIndicator={false}
+            contentContainerStyle={{ paddingHorizontal: 10 }}
+          >
+            <View style={styles.categoriesContainer}>
+              {categories.map((category) => {
+                const IconComponent =
+                  category.type === "Ionicons"
+                    ? Ionicons
+                    : category.type === "MaterialIcons"
+                    ? MaterialIcons
+                    : FontAwesome5;
+
+                return (
+                  <TouchableOpacity
+                    key={category.id}
+                    style={[
+                      styles.categoryItem,
+                      selectedCategory === category.name
+                        ? styles.selectedCategory
+                        : styles.unSelectedCategory,
+                    ]}
+                    onPress={() => setSelectedCategory(category.name)}
+                  >
+                    <View
+                      style={[
+                        styles.categoryIconContainer,
+                        selectedCategory === category.name
+                          ? { backgroundColor: "#FF9800" }
+                          : { backgroundColor: "#fff" },
+                      ]}
+                    >
+                      <IconComponent
+                        name={category.icon}
+                        size={30}
+                        color={
+                          selectedCategory === category.name
+                            ? "#fff"
+                            : "#FF9800"
+                        }
+                      />
+                    </View>
+                    <Text style={styles.categoryName}>{category.name}</Text>
+                  </TouchableOpacity>
+                );
+              })}
+            </View>
+          </ScrollView>
         </View>
 
         {/* Deals Section */}
@@ -85,189 +305,57 @@ const HomeScreen = ({ navigation }) => {
             </TouchableOpacity>
           </View>
 
-          <View style={styles.dealsContainer}>
-            {deals.map((deal) => (
-              <TouchableOpacity key={deal.id} style={styles.dealItem}>
-                <Image source={deal.image} style={styles.dealImage} />
-                <Text style={styles.dealName}>{deal.name}</Text>
-                <Text style={styles.dealPrice}>{deal.price}</Text>
-              </TouchableOpacity>
-            ))}
+          <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
+            <View style={styles.dealsContainer}>
+              {allDeals[selectedCategory]?.map((deal) => (
+                <TouchableOpacity key={deal.id} style={styles.dealItem}>
+                  <Image
+                    source={{ uri: deal.image }}
+                    style={styles.dealImage}
+                  />
+                  <Text style={styles.dealName}>{deal.name}</Text>
+                  <Text style={styles.dealPrice}>{deal.price}</Text>
+                </TouchableOpacity>
+              ))}
+            </View>
+          </ScrollView>
+        </View>
+
+        {/* Popular Section */}
+        <View style={styles.sectionContainer}>
+          <View style={styles.sectionHeader}>
+            <Text style={styles.sectionTitle}>Populars</Text>
+            <TouchableOpacity>
+              <Text style={styles.viewAllText}>View All</Text>
+            </TouchableOpacity>
+          </View>
+
+          <View style={styles.popularsContainer}>
+            {Object.values(populars)
+              .flat()
+              .reduce((acc, curr, index, array) => {
+                if (index % 2 === 0) acc.push(array.slice(index, index + 2));
+                return acc;
+              }, [])
+              .map((row, rowIndex) => (
+                <View key={rowIndex} style={styles.popularRow}>
+                  {row.map((popular) => (
+                    <TouchableOpacity key={popular.id} style={styles.dealItem}>
+                      <Image
+                        source={{ uri: popular.image }}
+                        style={styles.dealImage}
+                      />
+                      <Text style={styles.dealName}>{popular.name}</Text>
+                      <Text style={styles.dealPrice}>{popular.price}</Text>
+                    </TouchableOpacity>
+                  ))}
+                </View>
+              ))}
           </View>
         </View>
       </ScrollView>
     </SafeAreaView>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#FFFFFF",
-  },
-  header: {
-    flexDirection: "row",
-    alignItems: "center",
-    paddingHorizontal: 16,
-    paddingVertical: 10,
-    backgroundColor: "#FF9800",
-  },
-  searchContainer: {
-    flex: 1,
-    flexDirection: "row",
-    alignItems: "center",
-    backgroundColor: "#FFFFFF",
-    borderRadius: 30,
-    paddingHorizontal: 15,
-    height: 45,
-  },
-  searchIcon: {
-    marginRight: 10,
-  },
-  searchInput: {
-    flex: 1,
-    height: "100%",
-    fontSize: 16,
-    color: "#000000",
-  },
-  notificationButton: {
-    marginLeft: 15,
-  },
-  heroBanner: {
-    position: "relative",
-    height: 200,
-    marginHorizontal: 16,
-    marginTop: 16,
-    borderRadius: 16,
-    overflow: "hidden",
-  },
-  bannerImage: {
-    width: "100%",
-    height: "100%",
-  },
-  bannerOverlay: {
-    position: "absolute",
-    bottom: 0,
-    left: 0,
-    right: 0,
-    padding: 16,
-  },
-  bannerTitle: {
-    color: "#FFFFFF",
-    fontSize: 28,
-    fontWeight: "bold",
-    textShadow: "0px 2px 4px rgba(0, 0, 0, 0.5)",
-  },
-  bannerPrice: {
-    color: "#FFFFFF",
-    fontSize: 18,
-    fontWeight: "500",
-    textShadow: "0px 2px 4px rgba(0, 0, 0, 0.5)",
-  },
-  sectionContainer: {
-    marginTop: 24,
-    paddingHorizontal: 16,
-  },
-  sectionHeader: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-    marginBottom: 16,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: "bold",
-    color: "#333333",
-  },
-  viewAllText: {
-    fontSize: 16,
-    color: "#FF9800",
-    fontWeight: "bold",
-  },
-  categoriesContainer: {
-    flexWrap: "wrap",
-    flexDirection: "row",
-    justifyContent: "space-between",
-  },
-  categoryItem: {
-    alignItems: "center",
-    marginBottom: 16,
-    width: "22%",
-  },
-  categoryIconContainer: {
-    width: 70,
-    height: 70,
-    borderRadius: 35,
-    backgroundColor: "#FFFFFF",
-    justifyContent: "center",
-    alignItems: "center",
-    marginBottom: 8,
-    shadowColor: "#000",
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.1,
-    shadowRadius: 4,
-    elevation: 3,
-  },
-  categoryIcon: {
-    width: 36,
-    height: 36,
-    // tintColor: "#FF9800",
-  },
-  categoryName: {
-    fontSize: 14,
-    textAlign: "center",
-    color: "#333333",
-  },
-  dealsContainer: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-  },
-  dealItem: {
-    width: "48%",
-    borderRadius: 12,
-    overflow: "hidden",
-    marginBottom: 16,
-    shadowColor: "#000",
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.1,
-    shadowRadius: 4,
-  },
-  dealImage: {
-    width: "100%",
-    height: 150,
-    borderRadius: 12,
-  },
-  bottomNav: {
-    flexDirection: "row",
-    justifyContent: "space-around",
-    alignItems: "center",
-    height: 60,
-    borderTopWidth: 1,
-    borderTopColor: "#EEEEEE",
-    backgroundColor: "#FFFFFF",
-  },
-  navItem: {
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  navText: {
-    fontSize: 12,
-    marginTop: 4,
-    color: "#999999",
-  },
-  activeNavText: {
-    color: "#FF9800",
-    fontWeight: "bold",
-  },
-  dealName: {
-    fontSize: 14,
-    paddingLeft: 10,
-  },
-  dealPrice: {
-    paddingLeft: 10,
-    color: "#FF9800",
-    fontSize: 14,
-  },
-});
 
 export default HomeScreen;
---
+++ screens/home/style.jsx
@@ -0,0 +1,131 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#FFFFFF",
+  },
+  heroBanner: {
+    position: "relative",
+    height: 200,
+    marginHorizontal: 16,
+    marginTop: 16,
+    borderRadius: 16,
+    overflow: "hidden",
+  },
+  bannerImage: {
+    width: "100%",
+    height: "100%",
+  },
+  bannerOverlay: {
+    position: "absolute",
+    bottom: 0,
+    left: 0,
+    right: 0,
+    padding: 16,
+  },
+  bannerTitle: {
+    color: "#FFFFFF",
+    fontSize: 28,
+    fontWeight: "bold",
+  },
+  bannerPrice: {
+    color: "#FFFFFF",
+    fontSize: 18,
+    fontWeight: "500",
+  },
+  sectionContainer: {
+    marginTop: 24,
+    paddingHorizontal: 16,
+  },
+  sectionHeader: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 16,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "#333333",
+  },
+  viewAllText: {
+    fontSize: 16,
+    color: "#FF9800",
+    fontWeight: "bold",
+  },
+  categoriesContainer: {
+    flexDirection: "row",
+    paddingHorizontal: 10,
+    flexGrow: 1,
+  },
+  categoryItem: {
+    alignItems: "center",
+    marginBottom: 16,
+    marginRight: 16,
+  },
+  categoryIconContainer: {
+    width: 60,
+    height: 60,
+    borderRadius: 35,
+    backgroundColor: "#FFFFFF",
+    justifyContent: "center",
+    alignItems: "center",
+    marginBottom: 8,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 4,
+    elevation: 3,
+  },
+  categoryIcon: {
+    width: 36,
+    height: 36,
+  },
+  categoryName: {
+    fontSize: 14,
+    textAlign: "center",
+    color: "#333333",
+  },
+  dealsContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  dealItem: {
+    borderRadius: 12,
+    overflow: "hidden",
+    marginBottom: 16,
+    shadowColor: "#000",
+    shadowOffset: { width: 3, height: 2 },
+    shadowOpacity: 0.3,
+    shadowRadius: 4,
+    paddingBottom: 5,
+    marginRight: 10,
+  },
+  dealImage: {
+    width: 150,
+    height: 150,
+    borderRadius: 10,
+    borderBottomLeftRadius: 0,
+    borderBottomRightRadius: 0,
+    resizeMode: "cover",
+    backgroundColor: "#ccc",
+  },
+  dealName: {
+    fontSize: 14,
+    paddingVertical: 10,
+    paddingLeft: 10,
+  },
+  dealPrice: {
+    paddingLeft: 10,
+    color: "#FF9800",
+    fontSize: 14,
+  },
+  popularRow: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginBottom: 10,
+  },
+});
+
+export default styles;
